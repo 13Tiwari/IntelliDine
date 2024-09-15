d2b217e072311924a68cc8cb0bcea366ce1adae5
sanjays2024S <155586731+SanjayS598@users.noreply.github.com>
2024-09-14 23:37:45 -0400

    Adding my stuff

--- Backend/CrimeMappingML.py
+++ Backend/CrimeMappingML.py
@@ -10,3 +10,15 @@ import joblib
 
 ws = Workspace.from_config(path='config.json')
 #print(ws.name, ws.resource_group, ws.location, ws.subscription_id, sep = '\n')
+
+# Create an experiment
+experiment = Experiment(workspace=ws, name='crime-density-clustering')
+
+# Create a script run configuration
+env = Environment.from_conda_specification(name='sklearn-env', file_path='Backend/Azure/environment.yml')
+src = ScriptRunConfig(source_directory='Backend', script='train.py', compute_target='cpu-cluster', environment=env)
+
+# Submit the experiment
+run = experiment.submit(src)
+run.wait_for_completion(show_output=True)
+print("works")
--- Backend/train.py
+++ Backend/train.py
@@ -8,13 +8,9 @@ import joblib
 data = pd.read_csv('Backend/Data/CleanedData/cleaned_crime_data.csv')
 
 # Train a KMeans clustering model
-model = KMeans(n_clusters=5)
+model = KMeans(n_clusters=50)
 model.fit(data[['latitude', 'longitude']])
 
 # Save the model
 joblib.dump(model, 'crime_model.pkl')
 
-# Log the model to the run
-run = Run.get_context()
-run.upload_file(name='outputs/crime_model.pkl', path_or_stream='crime_model.pkl')
-run.complete()
